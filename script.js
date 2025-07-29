ab5dd4914597fe8fafa452e1ba0b5b1bb4d54081
maria eduarda <146000904+madumarrafon@users.noreply.github.com>
2025-07-29 11:37:47 -0300

    qqjddq

--- script.js
+++ script.js
@@ -18,35 +18,48 @@ const perguntas = [
             },
         ]
     },
+    
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial (IA), uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "Logo após sair de um dia cansativo como ferreiro na sua oficina, você se dirige a taverna mais próxima: O Buraco Fundo. Ao entrar, você percebe que o estabelecimento está lotado, mas há alguns lugares sobrando. Onde você senta? ",
         alternativas: [
-            "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-            "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
+            {
+            texto:"Sentar-se no balcão.",
+            afirmacao:"afirmação",
+            },
+            {
+            texto:"Sentar-se na janela.",
+            afirmacao:"afirmação",
+            },
         ]
     },
+
     {
-        enunciado: "Após a elaboração do trabalho, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "Logo após sair de um dia cansativo como ferreiro na sua oficina, você se dirige a taverna mais próxima: O Buraco Fundo. Ao entrar, você percebe que o estabelecimento está lotado, mas há alguns lugares sobrando. Onde você senta? ",
         alternativas: [
-            "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-            "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores."
+            {
+            texto:"Sentar-se no balcão.",
+            afirmacao:"afirmação",
+            },
+            {
+            texto:"Sentar-se na janela.",
+            afirmacao:"afirmação",
+            },
         ]
     },
+
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        enunciado: "Logo após sair de um dia cansativo como ferreiro na sua oficina, você se dirige a taverna mais próxima: O Buraco Fundo. Ao entrar, você percebe que o estabelecimento está lotado, mas há alguns lugares sobrando. Onde você senta? ",
         alternativas: [
-            "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-            "Criar uma imagem utilizando um gerador de imagem de IA."
+            {
+            texto:"Sentar-se no balcão.",
+            afirmacao:"afirmação",
+            },
+            {
+            texto:"Sentar-se na janela.",
+            afirmacao:"afirmação",
+            },
         ]
     },
-    {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda de uma IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz?",
-        alternativas: [
-           "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-            "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial."
-        ]
-    },
-];
 
 let atual = 0;
 let perguntaAtual;
